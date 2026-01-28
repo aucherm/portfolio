caef447bc883b97354e884b439df1d16ef9e3246
Maëlle Aucher <maelle.aucher@gmail.com>
2026-01-28 22:34:42 +0100

    feat : cards

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,11 +7,11 @@ export default function Portfolio() {
         Mes réalisations
       </h2>
 
-      <div className="container mx-auto grid gap-6 px-6 md:grid-cols-4 sm:grid-cols-2">
+      <div className="container mx-auto grid gap-6 px-6 md:grid-cols-3 sm:grid-cols-2">
         {projects.map((p, index) => (
           <div
             key={index}
-            className="relative w-full pt-[130%] rounded-lg overflow-hidden shadow-lg group"
+            className="relative w-full pt-[56.25%] rounded-lg overflow-hidden shadow-lg group"
           >
             {p.img && (
               <img
@@ -23,18 +23,18 @@ export default function Portfolio() {
 
             {/* Infos avec fond bleu semi-transparent */}
             <div className="absolute top-0 left-0 w-full h-full 
-                            bg-grey flex flex-col justify-center items-center text-center
+                            bg-green flex flex-col justify-center items-center text-center
                             opacity-0 transition-opacity duration-300 group-hover:opacity-100
                             p-4 rounded-lg">
-              <h3 className="text-xl font-audiowide mb-2 text-white">{p.title}</h3>
-              <p className="mb-1 text-sm text-white">{p.description}</p>
-              <p className="mb-2 text-sm font-semibold text-white">Technologies: {p.tech}</p>
+              <h3 className="text-xl font-audiowide mb-2 text-black">{p.title}</h3>
+              <p className="mb-1 text-sm text-black">{p.description}</p>
+              <p className="mb-2 text-sm font-semibold text-black">Technologies: {p.tech}</p>
               <div className="flex gap-4 mb-2">
                 <a
                   href={p.github}
                   target="_blank"
                   rel="noreferrer"
-                  className="underline text-white hover:text-mint transition-colors"
+                  className="underline text-black hover:text-mint transition-colors"
                 >
                   GitHub
                 </a>
@@ -43,7 +43,7 @@ export default function Portfolio() {
                     href={p.demo}
                     target="_blank"
                     rel="noreferrer"
-                    className="underline text-white hover:text-mint transition-colors"
+                    className="underline text-black hover:text-mint transition-colors"
                   >
                     Demo
                   </a>
