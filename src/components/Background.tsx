7a583d14f572c0a137f578529e19f2cec7c6f082
Maëlle Aucher <maelle.aucher@gmail.com>
2026-02-26 11:37:08 +0100

    style : center

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,18 +3,23 @@ import Button from "./Button";
 
 export default function Background() {
   return (
-    <div className="bg-[url('/mobile.png')]  md:bg-[url('/back.png')] bg-cover bg-center bg-no-repeat w-full h-screen font-audiowide ">
-      <h1 className="text-4xl pt-50 md:text-6xl md:pt-70 text-black pl-18 md:pl-40">MAËLLE AUCHER</h1>
-      <p className=" text-l md:text-2xl p-8 text-black pl-29 md:pl-65">DEVELOPPEUSE FULL STACK</p>
+    <div className="bg-[url('/mobile.png')] md:bg-[url('/back.png')] bg-cover bg-center bg-no-repeat w-full h-screen font-audiowide">
+      <div className="flex flex-col items-center justify-start text-center px-4 pt-40 md:items-start md:text-left md:pt-70 md:pl-40">
+        <h1 className="text-3xl md:text-6xl text-black">MAËLLE AUCHER</h1>
 
-      <div className="pl-22 md:pl-75 gap-4 mt-6 flex">
-        <Link to="/Myself">
-          <Button variant="primary">Get to know me</Button>
-        </Link>
+        <p className="text-lg md:text-2xl text-black mt-4 md:mt-4 md:pl-25">
+          DEVELOPPEUSE FULL STACK
+        </p>
 
-        <Link to="/Projects">
-          <Button variant="primary">My projects</Button>
-        </Link>
+        <div className="mt-6 flex flex-col gap-4 md:flex-row md:pl-35">
+          <Link to="/Myself">
+            <Button variant="primary">Get to know me</Button>
+          </Link>
+
+          <Link to="/Projects">
+            <Button variant="primary">My projects</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
